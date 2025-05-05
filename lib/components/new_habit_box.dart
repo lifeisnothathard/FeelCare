bc24bcc871ef62b8157b66f8818f5fb60bcd36d9
lifeisnothathard <qurratuadilah@gmail.com>
2025-05-05 14:31:24 +0800

    add new habit

---
+++ lib/components/new_habit_box.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class EnterNewHabitBox extends StatelessWidget {
+  final controller;
+  final VoidCallback onSave;
+  final VoidCallback onCancel;
+
+  const EnterNewHabitBox({
+    super.key,
+    required this.controller,
+    required this.onSave,
+    required this.onCancel,
+    });
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      backgroundColor: Colors.grey[900],
+      content: TextField(
+        controller: controller,
+        style: const TextStyle(color: Color.fromRGBO(255, 255, 255, 1)),
+        decoration: const InputDecoration(
+          enabledBorder: 
+              OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+          focusedBorder: 
+              OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+        ),
+      ),
+      actions: [
+        MaterialButton(
+          onPressed: onSave,
+          child: Text(
+            "Save",
+            style: TextStyle(color: Colors.white),
+          ),
+          color:Colors.black,
+          ),
+          MaterialButton(
+          onPressed: onCancel,
+          child: Text(
+            "Cancel",
+            style: TextStyle(color: Colors.white),
+          ),
+          color:Colors.black,
+          ),
+      ],
+    );
+  }
+}
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:feelcare/components/habit_tile.dart';
+import 'package:feelcare/components/my_fab.dart';
+import 'package:feelcare/components/new_habit_box.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -26,11 +28,49 @@ class HomePage extends StatefulWidget {
     }
     
     //create a new habit
+    final _newHabitController = TextEditingController();
+    void createNewHabit(){
+      //show alert dialog for user to enter the new habit details
+      showDialog(
+        context: context,
+        builder: (context){
+          return EnterNewHabitBox(
+            controller: _newHabitController,
+            onSave: saveNewHabit,
+            onCancel: cancelNewHabit,
+          );
+        },
+      );
+    }
+
+    //save new habit
+    void saveNewHabit(){
+      //add new habit to todays habit list
+      setState(() {
+        todaysHabitList.add([_newHabitController.text,false]);
+      });
+
+      //clear textfield
+      _newHabitController.clear();
+      //pop dialog box
+      Navigator.of(context).pop();
+    }
+    
+    //cancel new habit
+    void cancelNewHabit(){
+      //clear textfield
+      _newHabitController.clear();
+      //pop dialog box
+      Navigator.of(context).pop();
+    }
 
     @override
     Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[300],
+      floatingActionButton: MyFloatingActionButton(
+        onPressed: () => createNewHabit(),
+      ),
       body: ListView.builder(
         itemCount: todaysHabitList.length,
         itemBuilder: (context, index) {
